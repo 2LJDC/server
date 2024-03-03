94a6ae8f46a6c52cb406292935e8a374cd062209
cs97 <chrswr97@gmail.com>
2024-03-03 15:04:59 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,8 @@
 
 ip=$(ip a | grep eth0 | tail -n 1 | awk -F' ' '{ print $2 }' | awk -F'/' '{ print $1 }')
 
-echo $ip
+sed s/127.0.0.1/$ip/g src/main.rs > src/main2.rs 
 
+mv src/main2.rs src/main.rs 
+
+cargo build --release
