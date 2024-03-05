43aa8e9b8956fec7412ed590aae8468d8482ff21
cs97 <chrswr97@gmail.com>
2024-03-05 16:31:08 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -56,7 +56,7 @@ async fn index() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(afs::Files::new("/var/wwwstyle.css", "/var/www/style.css"))
+        App::new().service(afs::Files::new("/style.css", "/var/www/style.css"))
             .service(get_css)
             .service(index)
     })
