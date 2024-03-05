2b9a9950ce90aecaef5a34472d08e2e2d3cf6795
cs97 <chrswr97@gmail.com>
2024-03-05 15:21:19 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,25 @@ async fn get_css() -> impl Responder {
         .body(css)
 }
 
+#[get("/style.css")]
+async fn get_css() -> impl Responder {
+    let css = fs::read_to_string("/var/www/style.css").expect("Cannot read CSS file");
+    HttpResponse::Ok()
+        .content_type("text/css")
+        .body(css)
+}
+
+#[get("/srv-pic-2.webp")]
+async fn get_css() -> impl Responder {
+    let file = fs::NamedFile::open("/var/www/srv-pic-2.webp")?;
+    Ok(file
+        .use_last_modified(true)
+        .set_content_disposition(ContentDisposition {
+            disposition: DispositionType::Attachment,
+            parameters: vec![],
+        }))
+}
+
 #[get("/")]
 async fn index() -> impl Responder {
     let data = fs::read_to_string("/var/www/index.html").expect("Cannot read CSS file");
