be7db741a8bdb8a0a7bca3a0eeae24f8b3e43563
cs97 <chrswr97@gmail.com>
2024-03-08 02:18:57 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -150,8 +150,8 @@ async fn main() -> std::io::Result<()> {
 	    .service(cookies)
 	    //.service(get_logo)
 	    //.service(get_logo2x)
-	    .route("/Logo.png", ("/graphics/Logo.png", HttpResponse::Ok().content_type("image/vnd.microsoft.icon").body(std::fs::read("/var/www/graphics/Logo.png").expect("Cannot read webp file")))
-	    .route("/Logo.png", ("/graphics/Logo2x.png", HttpResponse::Ok().content_type("image/vnd.microsoft.icon").body(std::fs::read("/var/www/graphics/Logo2x.png").expect("Cannot read webp file")))
+	    .route("/Logo.png", ("/graphics/Logo.png", HttpResponse::Ok().content_type("image/vnd.microsoft.icon").body(std::fs::read("/var/www/graphics/Logo.png").expect("Cannot read webp file"))))
+	    .route("/Logo.png", ("/graphics/Logo2x.png", HttpResponse::Ok().content_type("image/vnd.microsoft.icon").body(std::fs::read("/var/www/graphics/Logo2x.png").expect("Cannot read webp file"))))
 
 	    //.service(afs::Files::new("/var/www", "."))
 
