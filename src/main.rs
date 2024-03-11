9eec1e27bb6da1921a1fd0e92ebccb343c341f40
cs97 <chrswr97@gmail.com>
2024-03-11 17:26:28 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,34 @@ use actix_web::{put, get, App, HttpResponse, HttpServer, Responder};
 use std::process::Command;
 use actix_files as fs;
 
+#[derive(serde::Deserialize)]
+pub struct Settings{
+	pub database: DatabaseSettings,
+	pub application_port: u16
+
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+	pub username: String,
+	pub password: String,
+	pub port: u16,
+	pub host: String,
+	pub database_name: String
+
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+	let settings = config::Config::builder()
+		.add_source(config::File::new("configuration.yaml", config::FileFormat::Yaml))
+		.build()?;
+	settings.try_deserialize::<Settings>()
+}
+
+
+
+
+
 // index
 #[get("/")]
 async fn index() -> impl Responder {
