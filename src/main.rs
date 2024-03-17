66d6bb0afe0b69463c25b70388e496be1753b0ac
cs97 <chrswr97@gmail.com>
2024-03-17 15:16:05 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -131,7 +131,7 @@ async fn main() -> std::io::Result<()> {
 	    .service(fs::Files::new("/", "/app/www"))
 	    
     })
-    .bind(("0.0.0.0", 8080))?
+    .bind(("0.0.0.0", 8000))?
     .run()
     .await
 }
