081a943b4a199914cd558b015713c0372ca66dd3
cs97 <chrswr97@gmail.com>
2024-03-17 14:40:44 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ git reset --hard HEAD
 
 git pull
 
-ip=$(ip a | grep eth0 | tail -n 1 | awk -F' ' '{ print $2 }' | awk -F'/' '{ print $1 }')
+#ip=$(ip a | grep eth0 | tail -n 1 | awk -F' ' '{ print $2 }' | awk -F'/' '{ print $1 }')
 
-sed s/127.0.0.1/$ip/g src/main.rs > src/main2.rs 
+#sed s/127.0.0.1/$ip/g src/main.rs > src/main2.rs 
 
-mv src/main2.rs src/main.rs 
+#mv src/main2.rs src/main.rs 
 
 cargo build --release
