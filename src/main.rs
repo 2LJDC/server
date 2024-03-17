d6810885bac4bfbda8c67ad7ae677ffcd4403245
cs97 <chrswr97@gmail.com>
2024-03-17 14:41:09 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -131,7 +131,7 @@ async fn main() -> std::io::Result<()> {
 	    .service(fs::Files::new("/", "/var/www"))
 	    
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
