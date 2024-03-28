ef186836a610f1a939e7bb40345410a3fe402fa5
cs97 <chrswr97@gmail.com>
2024-03-28 02:08:06 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@
 
 git pull
 cargo build --release
-cd www
-git pull
-cd ...
+rm -Rf www
+git clone https://github.com/2LJDC/Website_2.0
+mv Website_2.0 www
 podman build -t server .
 podman kill main-server; podman rm main-server
 podman run -dt --name main-server -p 0.0.0.0:443:8000 server
