6c221ecdadfa230114b8835be1b7d9b38f07420d
cs97 <chrswr97@gmail.com>
2024-03-28 02:06:07 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,9 @@
 
 git pull
 cargo build --release
+cd www
+git pull
+cd ...
 podman build -t server .
 podman kill main-server; podman rm main-server
 podman run -dt --name main-server -p 0.0.0.0:443:8000 server
